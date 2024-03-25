e0953c13dcf81541adb5736afbaca9468d32dc7e
Vlad Polewoi <vlad.polewoi1@gmail.com>
2024-03-25 17:41:42 +0200

    feat: :sparkles: footer

--- app/(marketing)/Footer.tsx
+++ app/(marketing)/Footer.tsx
@@ -1,7 +1,50 @@
+import { Button } from "@/components/ui/button"
+import Image from "next/image"
+
 export const Footer = () => {
+	const languages = [
+		{
+			id: 1,
+			name: "Croatian",
+			image: "/hr.svg",
+		},
+		{
+			id: 2,
+			name: "Spanish",
+			image: "/es.svg",
+		},
+		{
+			id: 3,
+			name: "French",
+			image: "/fr.svg",
+		},
+		{
+			id: 4,
+			name: "Italian",
+			image: "/it.svg",
+		},
+		{
+			id: 5,
+			name: "Japanese",
+			image: "/jp.svg",
+		},
+	]
+
 	return (
 		<footer className="hidden lg:block h-20 w-full border-t-2 border-slate-200 p-2">
-			Footer
+			<div className="max-w-screen-lg mx-auto flex items-center justify-evenly h-full">
+				{languages.map((item) => (
+					<Button className="w-full" size="lg" variant="ghost" key={item.id}>
+						<Image
+							src={item.image}
+							alt={item.name}
+							height={32}
+							width={40}
+							className="mr-4 rounded-md"
+						/>
+					</Button>
+				))}
+			</div>
 		</footer>
 	)
 }

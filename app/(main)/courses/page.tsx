a5d5715d936cde20e4882ce0e251a145171f727a
vlad polewoi <vlad.polewoi1@gmail.com>
2024-04-08 00:37:40 +0300

    First query

---
+++ app/(main)/courses/page.tsx
@@ -0,0 +1,14 @@
+import { getCourses } from '@/db/queries'
+
+const CoursesPage = async () => {
+  const data = await getCourses()
+
+  return (
+    <div className="h-full max-w-[912px] px-3 mx-auto">
+      <h1 className="text-2xl font-bold text-neutral-700">Language Courses</h1>
+      {JSON.stringify(data)}
+    </div>
+  )
+}
+
+export default CoursesPage

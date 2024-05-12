de5e1183dc692d8cdb3ddb654b231548d73f57ac
vlad polewoi <vlad.polewoi1@gmail.com>
2024-05-12 20:09:05 +0300

    feat: Add courses to db + migrate drizzle

--- components/FeedWrapper.tsx
+++ components/FeedWrapper.tsx
@@ -1,7 +1,7 @@
 type Props = {
-	children: React.ReactNode
+  children: React.ReactNode
 }
 
 export const FeedWrapper = ({ children }: Props) => {
-	return <div className="flex-1 relative top-0 pb-10">{children}</div>
+  return <div className="flex-1 relative top-0 pb-10">{children}</div>
 }
